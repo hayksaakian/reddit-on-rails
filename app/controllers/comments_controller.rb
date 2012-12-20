3893b5818c682ba9c4001de516763ff2d19ade6d
hayksaakian <fakest.id.ever@gmail.com>
2012-12-19 17:43:46 -0800

    view changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -48,7 +48,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.op, notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render action: "new" }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,15 @@
 class Comment
   include Mongoid::Document
+  field :title, type: String
   field :body, type: String
+
+  def op
+  	if self.parent
+  		self.parent.op
+  	else
+  		self
+  	end
+  end
 
   has_many :children, class_name: "Comment", inverse_of: :parent
   belongs_to :parent, class_name: "Comment", inverse_of: :children
